99ad3bc29283f0a8669a1f44f2a1cbb7aa3907a9
ChiragSareen7 <144792631+ChiragSareen7@users.noreply.github.com>
2023-10-05 17:13:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const $team2score = document.getElementById("score-team2")
 const $team2wickets = document.getElementById("wickets-team2")
 const resetbutton = document.getElementById("reset")
 const strikebutton = document.getElementById("strike")
+const strikeAudio = new Audio("http://bit.ly/so-ball-hit");
+const gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
 
 var team1score = 0
 var team2score = 0 
@@ -14,16 +16,40 @@ var turn = 1
 var ballsfaced = 0
 
 function finished() {
+  gameOverAudio.play();
     if(team1score>team2score) alert("India Wins");
     if(team2score>team1score) alert("Pakistan Wins");
-    else alert("Draw");
+    if(team1score==team2score) alert(" Its another super over");
 }
 
 const possibleoutcomes = [0, 1, 2, 3, 4, 5, 6, "W"]
 
 strikebutton.onclick = () => {
+
+  strikeAudio.pause();
+  strikeAudio.currentTime = 0;
+  strikeAudio.play();
+
     ballsfaced++;
-   if (turn === 1) {
+    if (turn === 2){
+      var score = possibleoutcomes[Math.floor(Math.random() * possibleoutcomes.length)]
+      console.log(score)
+      if (score === "W"){
+          team2wickets++;
+          $team2wickets.innerText = team2wickets
+          document.querySelector(`#team2-superover .ball:nth-child(${ballsfaced})`).innerHTML = score
+      }
+      else{
+          team2score += score
+          document.querySelector(`#team2-superover .ball:nth-child(${ballsfaced})`).innerHTML = score
+          $team2score.innerText = team2score
+      }
+      if(ballsfaced == 6 || team1wickets == 2 || team2score>team1score){
+        turn = 3
+        finished()
+      }
+  }
+  if (turn === 1) {
       var score = possibleoutcomes[Math.floor(Math.random() * possibleoutcomes.length)]
       console.log(score)
       if (score === "W"){
@@ -38,28 +64,11 @@ strikebutton.onclick = () => {
       }
       if(ballsfaced == 6 || team1wickets == 2){
         turn = 2
-        ballsfaced = 1
+        ballsfaced = 0
     
       }
    }
-   if (turn === 2){
-        var score = possibleoutcomes[Math.floor(Math.random() * possibleoutcomes.length)]
-        console.log(score)
-        if (score === "W"){
-            team2wickets++;
-            $team2wickets.innerText = team2wickets
-            document.querySelector(`#team2-superover .ball:nth-child(${ballsfaced})`).innerHTML = score
-        }
-        else{
-            team2score += score
-            document.querySelector(`#team2-superover .ball:nth-child(${ballsfaced})`).innerHTML = score
-            $team2score.innerText = team2score
-        }
-        if(ballsfaced == 6 || team1wickets == 2 || team2score>team1score){
-          turn = 2
-          finished()
-        }
-    }
+  
 }
 
 resetbutton.onclick = ()=>{window.location.reload()}
